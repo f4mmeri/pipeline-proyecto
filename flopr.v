24379c8a1d59e819f61980282d73d47e03c5663f
r3d <jose.alva.e@utec.edu.pe>
2026-06-14 21:52:25 -0500

    minor fix

--- flopr.v
+++ flopr.v
@@ -12,13 +12,3 @@ module flopr (input  clk, reset,
   end
 endmodule
 
-module flopenr #(parameter WIDTH = 8)(
-  input clk, reset, en,
-  input [WIDTH-1:0] d, 
-  output reg [WIDTH-1:0] q
-);
-  always @(posedge clk or posedge reset) begin
-    if(reset) q <= 0;
-    else if(en) q <= d;
-  end 
-endmodule
