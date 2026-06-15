5fd6e10bb9cd1a1176052ba3156d1a269c8c195f
r3d <jose.alva.e@utec.edu.pe>
2026-06-15 18:50:11 -0500

    top.v modificado para instanciar el nuevo riscv_pipeline, en lugar del single cycle.

--- top.v
+++ top.v
@@ -1,31 +1,33 @@
-module top(input  clk, reset, 
-           output [31:0] WriteData, DataAdr, 
-           output MemWrite);
+module top(
+    input         clk, reset, 
+    output [31:0] WriteData, DataAdr, 
+    output        MemWrite
+);
   
   wire [31:0] PC, Instr, ReadData; 
   
-  // instantiate processor and memories
-  riscvsingle rvsingle(
-    .clk(clk), 
-    .reset(reset), 
-    .PC(PC), 
-    .Instr(Instr), 
-    .MemWrite(MemWrite), 
-    .DataAdr(DataAdr), 
-    .WriteData(WriteData), 
-    .ReadData(ReadData)
+  // Instanciamos el procesador Pipelined
+  riscv_pipeline rvpipe(
+    .clk        (clk), 
+    .reset      (reset), 
+    .PCF        (PC), 
+    .InstrF     (Instr), 
+    .MemWriteM  (MemWrite), 
+    .ALUResultM (DataAdr), 
+    .WriteDataM (WriteData), 
+    .ReadDataM  (ReadData)
   ); 
 
   imem imem(
-    .a(PC), 
-    .rd(Instr)
+    .a  (PC), 
+    .rd (Instr)
   ); 
 
   dmem dmem(
-    .clk(clk), 
-    .we(MemWrite), 
-    .a(DataAdr), 
-    .wd(WriteData), 
-    .rd(ReadData)
+    .clk (clk), 
+    .we  (MemWrite), 
+    .a   (DataAdr), 
+    .wd  (WriteData), 
+    .rd  (ReadData)
   ); 
 endmodule
