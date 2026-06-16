10a52efb81f5d22780764eef7c186cd06526defe
r3d <jose.alva.e@utec.edu.pe>
2026-06-15 21:23:45 -0500

    waveforms

--- imem.v
+++ imem.v
@@ -4,7 +4,7 @@ module imem(input  [31:0] a,
   reg [31:0] RAM[63:0]; 
 
   initial begin
-      $readmemh("test1_nodep.mem",RAM); 
+      $readmemh("test4_flushing.mem",RAM); 
   end
 
   assign rd = RAM[a[31:2]]; // word aligned
